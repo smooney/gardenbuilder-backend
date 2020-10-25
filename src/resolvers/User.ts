534d487d61516b0a2335c35c75656ec13a1e6b61
David Thompson <capndavet@gmail.com>
2020-10-24 19:39:57 -0500

    made createUser mutation

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,11 +1,26 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { Resolver, Query } from 'type-graphql'
+import { Resolver, Query, InputType, Field, Mutation, Arg } from 'type-graphql'
 import { User } from '../entities/User'
+
+@InputType()
+class EmailPasswordInput {
+  @Field()
+  email: string
+  @Field()
+  password: string
+}
 
 @Resolver()
 export class UserResolver {
   @Query(() => [User])
   users() {
     return User.find()
+  }
+
+  @Mutation(() => User)
+  async createUser(@Arg('options') options: EmailPasswordInput) {
+    const user = User.create(options)
+    await user.save()
+    return user
   }
 }
