56728a8ac05d24d6c783c714664cb13acaed0543
David Thompson <capndavet@gmail.com>
2020-10-24 21:39:33 -0500

    made method to quickly return the error portion of a response

---
+++ src/libs/errorResponse.ts
@@ -0,0 +1,12 @@
+import { Response } from '../types/Response'
+
+/**
+ * Return an Response object with an error > message property
+ * @param [string] errorMessage
+ * @returns [Object]
+ */
+export const errorResponse = (errorMessage: string): Response => ({
+  error: {
+    message: errorMessage,
+  },
+})
