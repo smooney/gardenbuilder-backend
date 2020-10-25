bf95ab92e6211e07eb556fa98f189d91d589897b
David Thompson <capndavet@gmail.com>
2020-10-24 21:41:11 -0500

    made Response type which will be the basis of all our responses

---
+++ src/types/Response.ts
@@ -0,0 +1,8 @@
+import { Field, ObjectType } from 'type-graphql'
+import { Error } from '../types/Error'
+
+@ObjectType()
+export class Response {
+  @Field(() => Error, { nullable: true })
+  error?: Error
+}
