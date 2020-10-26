d1be85d33175eb389c54c38966fb111143306f01
David Thompson <capndavet@gmail.com>
2020-10-26 18:51:17 -0500

    added authChecker block to createSchema

--- src/libs/createSchema.ts
+++ src/libs/createSchema.ts
@@ -4,5 +4,11 @@ import { GraphQLSchema } from 'graphql'
 export async function createSchema(): Promise<GraphQLSchema> {
   return await buildSchema({
     resolvers: [__dirname + '/../resolvers/*.ts'],
+    authChecker: ({ context: { req } }, roles) => {
+      if (req.session.userId) {
+        return true
+      }
+      return false
+    },
   })
 }
