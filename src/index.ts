79d6a836d3fc7d82fd8327c08f5a9b2ec0c23cc3
David Thompson <capndavet@gmail.com>
2020-10-28 11:33:16 -0500

    Added cors to server initialization

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import { createConnection } from 'typeorm'
 import { ApolloServer } from 'apollo-server'
 import { createSchema } from './libs/createSchema'
 
-async function main() {
+const PORT = 8000
+
+async function setupAndRunServer() {
   const connection = await createConnection()
   const schema = await createSchema()
-  const server = new ApolloServer({ schema })
-  await server.listen(8000)
-  console.log('Server has started!')
+  const devMode = process.env.NODE_ENV === 'development'
+  const server = new ApolloServer({ cors: devMode, schema })
+  await server.listen(PORT)
+  console.log(`Server has started on port ${PORT}`)
 }
-main()
+
+setupAndRunServer()
