8398b3a579206f6de9f6ffff1c1b939beed899fb
dthompson <capndavet@gmail.com>
2020-12-12 16:00:56 -0600

    commented out code queryTrefle bc its a nuisance right now

--- src/utils/queryTrefle.ts
+++ src/utils/queryTrefle.ts
@@ -6,16 +6,17 @@ const { TREFLE_ACCESS_TOKEN } = process.env
 
 export async function getPlantTypes() {
   try {
-    const result = await axios.get(
-      `https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null&token=${TREFLE_ACCESS_TOKEN}`
-    )
-    const plants = result.data.data.map(
-      ({ id, common_name }: { id: number; common_name: string }) => ({
-        id,
-        common_name,
-      })
-    )
-    return plants
+    // const result = await axios.get(
+    //   `https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null&token=${TREFLE_ACCESS_TOKEN}`
+    // )
+    // const plants = result.data.data.map(
+    //   ({ id, common_name }: { id: number; common_name: string }) => ({
+    //     id,
+    //     common_name,
+    //   })
+    // )
+    // return plants
+    return {}
   } catch (err) {
     console.log(err)
   }
