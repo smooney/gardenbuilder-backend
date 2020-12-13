1bb3940e3e3aa214a9fe466b0b1f12e0b12dc8f7
dthompson <capndavet@gmail.com>
2020-12-12 20:52:21 -0600

    removed coverage reports from jest.config.js and removed coverage directory

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,4 @@ module.exports = {
     '^.+\\.ts?$': 'ts-jest',
   },
   testEnvironment: 'node',
-  coverageThreshold: {
-    global: {
-      branches: 40,
-      functions: 40,
-      lines: 40,
-      statements: 40
-    }
-  },
-  coverageReporters: ['json', 'lcov'],
 }
