daff94beadc2c7b2d804b135e388caf0f3746e32
David Thompson <capndavet@gmail.com>
2021-02-18 18:51:41 -0600

    remove unneeded console.logs

--- src/utils/getUserIdFromRequest.ts
+++ src/utils/getUserIdFromRequest.ts
@@ -4,9 +4,11 @@ import jwt from './jwt'
 export function getUserIdFromRequest(
   req: RequestWithAuthenticationHeader
 ): number | null {
-  const token = req?.headers?.authorization
+  const token = req?.headers?.authorization?.split(' ')[1]
   if (token) {
+    console.log(`about to get userId`)
     const userId = jwt.getUserId(token)
+    console.log(userId)
     return userId
   }
   return null
