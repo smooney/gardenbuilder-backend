a32887eeb220da8d5fecdbbd9992c63cb3d7a8a8
David Thompson <capndavet@gmail.com>
2021-05-01 18:48:33 -0500

    remove queryTrefle util

--- src/resolvers/Variety.ts
+++ src/resolvers/Variety.ts
@@ -1,21 +1,20 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { errorResponse } from '../utils'
-import { Arg, Resolver, Query } from 'type-graphql'
-import { Variety, VarietiesResponse } from '../types'
-import { insertVarietiesIntoDatabase, getVarieties } from '../utils/queryTrefle'
+// import { errorResponse } from '../utils'
+// import { Arg, Resolver, Query } from 'type-graphql'
+// import { Variety, VarietiesResponse } from '../types'
 
-@Resolver()
-export class VarietiesResolver {
-  @Query(() => VarietiesResponse)
-  async varieties(@Arg('species', () => String) species: string) {
-    try {
-      const varieties: Variety[] = await getVarieties(species)
-      // TODO: Insert varieties into database
-      insertVarietiesIntoDatabase(varieties)
+// @Resolver()
+// export class VarietiesResolver {
+//   @Query(() => VarietiesResponse)
+//   async varieties(@Arg('species', () => String) species: string) {
+//     try {
+//       const varieties: Variety[] = await getVarieties(species)
+//       // TODO: Insert varieties into database
+//       insertVarietiesIntoDatabase(varieties)
 
-      return { varieties }
-    } catch (err) {
-      return errorResponse(err.message)
-    }
-  }
-}
+//       return { varieties }
+//     } catch (err) {
+//       return errorResponse(err.message)
+//     }
+//   }
+// }
