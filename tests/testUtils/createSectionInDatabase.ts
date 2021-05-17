3941f8667c00219727d9c62b2192985d5deab786
David Thompson <capndavet@gmail.com>
2021-05-17 15:48:08 -0500

    update faker call

--- tests/testUtils/createSectionInDatabase.ts
+++ tests/testUtils/createSectionInDatabase.ts
@@ -3,8 +3,8 @@ import { Bed, Section } from '../../src/entities'
 
 export async function createSectionInDatabase(
   bed: Bed,
-  xPosition: number = faker.random.number({ min: 0, max: 10 }),
-  yPosition: number = faker.random.number({ min: 0, max: 10 }),
+  xPosition: number = faker.datatype.number({ min: 0, max: 10 }),
+  yPosition: number = faker.datatype.number({ min: 0, max: 10 }),
   plantType = 'Tomato'
 ): Promise<Section> {
   const section = Section.create({
